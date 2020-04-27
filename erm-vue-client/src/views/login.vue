51e5d78eedc96671fa5e120d7d7041696ec2b384
Sandra <2598331514@qq.com>
2020-04-27 17:16:38 +0800

    press enter to login

--- erm-vue-client/src/views/login.vue
+++ erm-vue-client/src/views/login.vue
@@ -1,12 +1,12 @@
 <template>
-	<div class="box">
-		<h2>CRM客户管理系统</h2>
-		<h3>(用户名：admin, 密码：666)</h3>
+	<div class="login">
+		<h2>员工管理系统</h2>
 		<div class="inputs">
 			<el-input v-model="username" placeholder="请输入用户名" prefix-icon="el-icon-user" class="username"></el-input>
 			<el-input v-model="password" type="password" placeholder="请输入密码" prefix-icon="el-icon-lock"></el-input>
 			<el-button type="primary" class="button" @click="login">登录</el-button>
 		</div>
+		<p>可用用户名：admin, 密码：666</p>
 	</div>
 </template>
 
@@ -21,7 +21,14 @@
 				password: ""
 			}
 		},
+		created(){
+			document.addEventListener("keypress", this.enterEvent, false);
+		},
 		methods: {
+			enterEvent(event){
+				if (event.keyCode === 13)
+					this.login();
+			},
 			login(){
 				if (!this.username || !this.password){
 					this.$message.error("用户名或密码不能为空！");
@@ -33,15 +40,19 @@
 							confirmButtonText: "确定"
 						});
 					} else if (data.code == 0){
-						this.$alert("登录成功", "提示", {
-							confirmButtonText: "确定",
-							callback: action=>{// action可以判断点击了哪个按钮
-								// 切换到"/"
-								this.$router.push("/");
-								// 缓存power
-								localStorage.setItem("power", data.power);
-							}
-						});
+						this.$message("登录成功");
+						// 缓冲一下，跳转不那么突然
+						let timeout = setTimeout(_ =>{
+							// 切换到"/"
+							this.$router.push("/");
+							// 缓存power
+							localStorage.setItem("power", data.power);
+
+							clearTimeout(timeout);
+
+							// 清除监听器，这个监听器只能在当前登录组件内使用
+							document.removeEventListener("keypress", this.enterEvent);
+						}, 1500);
 					}
 				});
 			}
@@ -50,7 +61,7 @@
 </script>
 
 <style scoped lang="less">
-	.box {
+	.login {
 		width: 500px;
 		margin: 100px auto;
 		text-align: center;
@@ -59,9 +70,6 @@
 			line-height: 50px;
 		}
 
-		h3 {
-			font-size: 15px;
-		}
 		// 为组件设置样式时，不要用标签选择器，会被替换
 		.inputs {
 			width: 300px;
@@ -79,7 +87,7 @@
 
 		p {
 			position: absolute;
-			bottom: 5px;
+			bottom: 50px;
 			left: 50%;
 			transform: translateX(-50%);
 			font-size: 14px;
